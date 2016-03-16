52c024d2e88830830c95ecd9593927aae5ae54c5
황인용 <hiy2259@me.com>
2016-03-16 09:48:07 +0900

    React.js hello world 예제 추가

---
+++ getting-started/hiy2259/src/sample.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Hello = React.createClass({
+  displayName: 'Hello',
+
+  render: function () {
+    return React.createElement('div', null, 'Hello ', this.props.name);
+  }
+});
+
+ReactDOM.render(React.createElement(Hello, { name: 'World' }), document.getElementById('container'));
